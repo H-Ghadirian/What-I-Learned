91ccd804906ea32e6a15f218faaaa6f3cf808357
Hamed Ghadirian <H.ghadirian@outlook.com>
2023-02-25 23:00:47 +0100

    Add TabView

--- WhatILearned/ContentView.swift
+++ WhatILearned/ContentView.swift
@@ -8,31 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    private var secondTab: some View {
+        VStack {
+            NavigationLink(destination: {
+                Text("Second View")
+                    .navigationTitle("Second")
+                    .navigationBarTitleDisplayMode(.inline)
+            }, label: {
+                Text("Open Second View!")
+            })
+            
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("This is my resume App")
+            Text("This app is all about what I learned!")
+            Text("You can find the code on")
+            Link(
+                "My Github",
+                destination: URL(
+                    string: "https://github.com/H-Ghadirian/What-I-Learned"
+                )!
+            )
+        }
+    }
+    
     var body: some View {
         NavigationView {
-            VStack {
-                NavigationLink(destination: {
-                    Text("Second View")
-                        .navigationTitle("Second")
-                        .navigationBarTitleDisplayMode(.inline)
-                }, label: {
-                    Text("Open Second View!")
-                })
-
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("This is my resume App")
-                Text("This app is all about what I learned!")
-                Text("You can find the code on")
-                Link(
-                    "My Github",
-                    destination: URL(
-                        string: "https://github.com/H-Ghadirian/What-I-Learned"
-                    )!
-                )                
+            TabView {
+                Text("First View")
+                    .padding()
+                    .tabItem {
+                        Image(systemName: "1.circle")
+                        Text("First")
+                    }
+                    .tag(1)
+                secondTab
+                    .padding()
+                    .tabItem {
+                        Image(systemName: "2.circle")
+                        Text("Second")
+                    }
+                    .tag(2)
             }
-            .padding()
             .navigationBarTitle("Hamed Resume")
         }
     }
