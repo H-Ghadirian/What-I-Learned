d02a2ded77345ad28c0c5d6166d303cf1846350d
Hamed Ghadirian <H.ghadirian@outlook.com>
2023-08-28 23:10:13 +0200

    Add some space on top of the screen in order to dismiss in sheet

--- WhatILearned/Projects/Map/MapView.swift
+++ WhatILearned/Projects/Map/MapView.swift
@@ -18,29 +18,35 @@ struct MapView: View {
     let locationManager = CLLocationManager()
 
     var body: some View {
-        Map(
-            coordinateRegion: $region,
-            showsUserLocation: true,
-            userTrackingMode: .constant(.follow)
-        )
-        .onAppear {
-            locationManager.requestWhenInUseAuthorization()
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                Map(
+                    coordinateRegion: $region,
+                    showsUserLocation: true,
+                    userTrackingMode: .constant(.follow)
+                )
+                .frame(height: geometry.size.height - 50)
+                .onAppear {
+                    locationManager.requestWhenInUseAuthorization()
+                }
+            }
+
+    //        Map(
+    //            coordinateRegion: .constant(
+    //                region
+    //            ),
+    //            interactionModes: []
+    //        )
+    //        .frame(width: 400, height: 300)
+
+    //        Map(
+    //            coordinateRegion: .constant(
+    //                region
+    //            ),
+    //            interactionModes: [.zoom]
+    //        )
+    //        .frame(width: 400, height: 300)
         }
-
-//        Map(
-//            coordinateRegion: .constant(
-//                region
-//            ),
-//            interactionModes: []
-//        )
-//        .frame(width: 400, height: 300)
-
-//        Map(
-//            coordinateRegion: .constant(
-//                region
-//            ),
-//            interactionModes: [.zoom]
-//        )
-//        .frame(width: 400, height: 300)
     }
 }
