f7363a1feb9a586cc0883fd6958c912f4f717964
Hamed Ghadirian <H.ghadirian@outlook.com>
2023-08-30 12:57:25 +0200

    Add tab view

---
+++ WhatILearned/Projects/TabView/TestTabView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct TestTabView: View {
+    @State var selectedTab = 0
+
+    var body: some View {
+        TabView {
+            Text("Hello World")
+            Text("Hello World1")
+            Text("Hello World2")
+        }
+        .tabViewStyle(.page)
+        .indexViewStyle(.page(backgroundDisplayMode: .always))
+    }
+}
