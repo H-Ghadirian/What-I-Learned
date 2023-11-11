cc4c74189b3bd1ca51d8a908610f671d7d7d582d
Hamed Ghadirian <H.ghadirian@outlook.com>
2023-11-12 00:15:52 +0100

    refactor

--- WhatILearned/SearchableListViewModel.swift
+++ WhatILearned/SearchableListViewModel.swift
@@ -8,71 +8,25 @@
 import SwiftUI
 
 class SearchableListViewModel {
-    private var projects: [any ProjectProtocol] = [
-        PlayingWithNotifications.project(),
-        CustomizeButtonsView.project(),
-        ToggleExperiment.project(),
-        ListInAdjustableSheetView.project(),
-        KerningView.project(),
-        DemoFloatingTextEditor.project(),
-        DemoFloatingTextField.project(),
-        ToastViewExample.project(),
-        EssentialsCounterView.project(),
-        RatingScreenView.project(),
-        OldSheetView.project(),
-        UserDefaultsView.project(),
-        TheUIViewControllerRepresentable.project(),
-        SheetNavigation.project(),
-        AsyncAwaitRequest.project(),
-        CounterAppView.project(),
-        FakeNewsFeedView.project(),
-        MenuView<MenuViewModel>.project(),
-        LottieContentView.project(),
-        TestTabView.project(),
-        CollectionViewContent.project(),
-        HapticTestView.project(),
-        TestMyViewModifiersView.project(),
-        TestMacro.project(),
-        KeychainView.project(),
-        MapView.project(),
-        UsersView.project()
-    ]
-    init() {
-        if #available(iOS 15.0, *) {
-            projects.append(contentsOf:
-                                [
-                                    ContinuationContentView.project(),
-                                    AddArticleView.project(),
-                                    DismissKeyboardView.project()
-                                ]
-            )
-        }
-        if #available(iOS 16.0, *) {
-            projects.append(contentsOf:
-                                [
-                                    TestSheets.project(),
-                                    SecondJsonMenuListView.project(),
-                                    JsonMenuListView.project(),
-                                    ChartsView.project(),
-                                    PhotoPickerView.project(),
-                                    ChrisNavigation.project(),
-                                    OnTapGestureLocation.project()
-                                ]
-            )
-        }
-        if #available(iOS 17.0, *) {
-            projects.append(contentsOf:
-                                [
-                                    BackgroundOverlayView.project(),
-                                    BookmarkTipView.project()
-                                ]
-            )
-        }
-    }
     private var searchResultProjects: [any ProjectProtocol] = []
+    private var projects: [any ProjectProtocol] = []
 
     var title: String { "Projects \(numberOfProjects)" }
     var numberOfProjects: Int { projects.count }
+    var iOSVersion: Int {
+        Int(
+            Double(
+                UIDevice.current.systemVersion.split(separator: ".").first ?? "0"
+            ) ?? 0
+        )
+    }
+
+    init() {
+        if #available(iOS 17.0, *) { projects.append(contentsOf: Projects.iOS17) }
+        if #available(iOS 16.0, *) { projects.append(contentsOf: Projects.iOS16) }
+        if #available(iOS 15.0, *) { projects.append(contentsOf: Projects.iOS15) }
+        projects.append(contentsOf: Projects.iOS14)
+    }
 
     func iOSVersionOf(_ index: Int) -> Int {
         searchResultProjects[index].tags.version.rawValue
@@ -81,6 +35,7 @@ class SearchableListViewModel {
     func presentationModeOf(project index: Int) -> PresentationMode {
         searchResultProjects[index].presentationMode
     }
+
     func colorOf(project index: Int) -> Color {
         switch searchResultProjects[index].tags.version {
         case .iOS14:
@@ -93,9 +48,11 @@ class SearchableListViewModel {
             return .yellow
         }
     }
+
     func viewOf(project index: Int) -> AnyView {
         type(of: searchResultProjects[index]).run()
     }
+
     func titleOf(project index: Int) -> String {
         "\(index + 1) - " + searchResultProjects[index].name
     }
@@ -107,13 +64,5 @@ class SearchableListViewModel {
             searchResultProjects = projects.filter { $0.name.contains(text) }
         }
         return searchResultProjects.map { $0.name }
-    }
-
-    var iOSVersion: Int {
-        Int(
-            Double(
-                UIDevice.current.systemVersion.split(separator: ".").first ?? "0"
-            ) ?? 0
-        )
     }
 }
---
+++ WhatILearned/Shared/Project/Projects.swift
@@ -0,0 +1,64 @@
+//
+//  Projects.swift
+//  WhatILearned
+//
+//  Created by Ghadirian, Hamed, HSE DE on 12.11.23.
+//
+
+import Foundation
+enum Projects {
+
+    @available(iOS 17.0, *)
+    static let iOS17: [any ProjectProtocol] = [
+        BackgroundOverlayView.project(),
+        BookmarkTipView.project()
+    ]
+
+    @available(iOS 16.0, *)
+    static let iOS16: [any ProjectProtocol] = [
+        TestSheets.project(),
+        SecondJsonMenuListView.project(),
+        JsonMenuListView.project(),
+        ChartsView.project(),
+        PhotoPickerView.project(),
+        ChrisNavigation.project(),
+        OnTapGestureLocation.project()
+    ]
+
+    @available(iOS 15.0, *)
+    static let iOS15: [any ProjectProtocol] = [
+        ContinuationContentView.project(),
+        AddArticleView.project(),
+        DismissKeyboardView.project()
+    ]
+
+    static let iOS14: [any ProjectProtocol] = [
+        PlayingWithNotifications.project(),
+        CustomizeButtonsView.project(),
+        ToggleExperiment.project(),
+        ListInAdjustableSheetView.project(),
+        KerningView.project(),
+        DemoFloatingTextEditor.project(),
+        DemoFloatingTextField.project(),
+        ToastViewExample.project(),
+        EssentialsCounterView.project(),
+        RatingScreenView.project(),
+        OldSheetView.project(),
+        UserDefaultsView.project(),
+        TheUIViewControllerRepresentable.project(),
+        SheetNavigation.project(),
+        AsyncAwaitRequest.project(),
+        CounterAppView.project(),
+        FakeNewsFeedView.project(),
+        MenuView<MenuViewModel>.project(),
+        LottieContentView.project(),
+        TestTabView.project(),
+        CollectionViewContent.project(),
+        HapticTestView.project(),
+        TestMyViewModifiersView.project(),
+        TestMacro.project(),
+        KeychainView.project(),
+        MapView.project(),
+        UsersView.project()
+    ]
+}
